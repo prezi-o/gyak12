d6f549be99833414c9cd5ff285ae0f4fb0d7ae4a
prezi-o <oli.prezi@gmail.com>
2023-12-05 10:34:52 +0100

    frissítés

--- gyak12/Form1.cs
+++ gyak12/Form1.cs
@@ -1,10 +1,60 @@
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+
 namespace gyak12
 {
     public partial class Form1 : Form
     {
+        Excel.Application xlApp;
+        Excel.Workbook xlWb;
+        Excel.Worksheet xlSheet;
         public Form1()
         {
             InitializeComponent();
+
+            try
+            {
+                xlApp = new Excel.Application();
+
+                xlWb = xlApp.Workbooks.Add(Missing.Value);
+
+                xlSheet = xlWb.ActiveSheet;
+
+                //CreateTable();
+
+                xlApp.Visible= true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWb.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWb = null;
+                xlApp = null;
+            }
         }
+
+        void CreateTable()
+        {
+            string[] fejlecek = new string[]
+            {
+                "Kérdés",
+                "1. válasz",
+                "2. válasz",
+                "3. válasz",
+                "Helyes válasz",
+                "kép"
+            };
+
+            for (int i = 0; i < fejlecek.Length; i++)
+            {
+                xlSheet.Cells[1, 1] = fejlecek[0];
+            }
+        }
+
     }
+
 }
